0de2e20b1c68ad7da591a3f1764c117c29a3306c
asidikfauzi <asidikfauzi@gmail.com>
2023-09-03 03:14:36 +0700

    FEATURE - a new Feature model categories

---
+++ models/categories.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Categories struct {
+	CategoryID   uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primary_key;column:category_id;" json:"category_id"`
+	CategoryName string     `gorm:"type:varchar(120);not null" json:"category_name"`
+	CreatedAt    time.Time  `gorm:"default:null"`
+	UpdatedAt    *time.Time `gorm:"default:null"`
+	DeleteAt     *time.Time `gorm:"default:null"`
+}
